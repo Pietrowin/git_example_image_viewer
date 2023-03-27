f35b8a69a00ab41ef13c16b07ec042a053a62be4
pietropinzani <pietro.pinzani@stud.unifi.it>
2023-03-27 17:29:53 +0200

    Added support for channel and depth

---
+++ version.h
@@ -0,0 +1,11 @@
+//
+// Created by Pietro Pinzani on 27/03/23.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+static const std::string version = "0.1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
